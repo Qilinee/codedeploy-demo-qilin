b3a9b7d3be8c399578e692bd6e6bac058727ce00
Alash-WorkLaptop <alash@precisionit.com.au>
2016-07-25 15:04:12 +1000

    another commit

--- before_install.sh
+++ before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check for ansible tools, and attempt to install if not-force
+# Check for ansible tools, and attempt to install if not
 sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update -y
