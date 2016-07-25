e27d7c790a3301eb8d573751f478d462d8289b5a
Alash-WorkLaptop <alash@precisionit.com.au>
2016-07-25 14:45:04 +1000

    adding auto yes to ansible installs

--- before_install.sh
+++ before_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Check for ansible tools, and attempt to install if not
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update -y
+sudo apt-get install ansible -y
 
 # install ansible prereqs and run playbook
 ansible-playbook -i "localhost" configureserver.yml
